a71a517240af9b44520dddb1e319a95ea8f1411f
YOSHIFUJI Hideaki <hideaki.yoshifuji@miraclelinux.com>
2014-12-16 20:34:06 +0900

    license: Change hatohol license from GPLv2 to GPLv3 with exception/LGPLv3.
    We change hatohol license from GPLv2 to GPLv3 with exception,
    and LGPLv3.
    - To accept maximum collaborators.
    - To allow (indirect) linkage with OpenSSL via MariaDB.
    
    Following files are released under LGPLv3:
    	server/common/*
    	server/hap/Hap*
    	server/hap/Hatohol*
    	server/mlpl/src/*
    	server/tools/hatohol-def-src-file-generator.cc
    
    Other source files are released under GPLv3 with "OpenSSL exception":
    	Additional permission under GNU GPL version 3 section 7
    
    	If you modify this program, or any covered work, by linking or
    	combining it with the OpenSSL project's OpenSSL library (or a
    	modified version of that library), containing parts covered by the
    	terms of the OpenSSL or SSLeay licenses, Project Hatohol
    	grants you additional permission to convey the resulting work.
    	Corresponding Source for a non-source form of such a combination
    	shall include the source code for the parts of OpenSSL used as well
    	as that of the covered work.
    
    Signed-off-by: YOSHIFUJI Hideaki <hideaki.yoshifuji@miraclelinux.com>
    Signed-off-by: YOSHIFUJI Hideaki <hideaki@yoshifuji.org>

--- server/src/IncidentSender.h
+++ server/src/IncidentSender.h
@@ -4,9 +4,8 @@
  * This file is part of Hatohol.
  *
  * Hatohol is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU General Public License, version 3
+ * as published by the Free Software Foundation.
  *
  * Hatohol is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,6 +14,17 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Hatohol. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7
+ *
+ * If you modify this program, or any covered work, by linking or
+ * combining it with the OpenSSL project's OpenSSL library (or a
+ * modified version of that library), containing parts covered by the
+ * terms of the OpenSSL or SSLeay licenses, Project Hatohol
+ * grants you additional permission to convey the resulting work.
+ * Corresponding Source for a non-source form of such a combination
+ * shall include the source code for the parts of OpenSSL used as well
+ * as that of the covered work.
  */
 
 #ifndef IncidentSender_h
