3829cce0e36539a252e1233ba6f3dbabc8848cf9
Masayuki Nakagawa <masayuki.nakagawa@miraclelinux.com>
2015-04-28 17:43:51 +0900

    Update 4/28 PM
    I fix grammer to follow pep8 later.

--- server/hap2/zabbixapi.py
+++ server/hap2/zabbixapi.py
@@ -27,15 +27,8 @@ class ZabbixAPI:
 		return res_dict["result"]
 
 
-	self.hosts = []
-	self.host_group_membeship = []
 	# The following method gets not only hosts info but also host group membership.
-	def get_hosts(self, prev):
-		prev.hosts = self.hosts
-		prev.host_group_membership = self.host_group_membership
-		self.hosts = []
-		self.host_group_membership = []
-
+	def get_hosts(self):
 		post = json.dumps({"jsonrpc":"2.0", "method":"host.get", "params":{"output":"extend", "selectGroups":"refer", "monitored_hosts":True}, "auth":self.auth_token, "id":1})
 		request = urllib2.Request(self.url, post, self.HEADER)
 		response = urllib2.urlopen(request)
@@ -46,14 +39,18 @@ class ZabbixAPI:
 		if not self.result:
 			return
 
+		hosts = []
+		host_group_membership = []
 		for host in res_dict["result"]:
-			self.hosts.append({host["hostid"]:host["name"]})
+			hosts.append({"hostId":host["hostid"], "hostName":host["name"]})
 
 			group_ids = []
 			for groups in host["groups"]:
 				group_ids.append(groups["groupid"])
 
-			self.host_group_membership.append({host["id"]:group_ids})
+			host_group_membership.append({"hostId":host["id"], "groupIds":group_ids})
+
+		return (hosts, host_group_membership)
 
 
 	def get_host_groups(self):
@@ -67,25 +64,17 @@ class ZabbixAPI:
 		if not self.result:
 			return
 
-		for host in res_dict["result"]:
-			self.hosts.append({host["hostid"]:host["name"]})
+		host_groups = []
+		for host_group in res_dict["result"]:
+			host_groups.append({"hostId":host_group["hostid"], "groupName":host_group["name"]})
 
-			group_ids = []
-			for groups in host["groups"]:
-				group_ids.append(groups["groupid"])
-
-			self.host_group_membership.append({host["id"]:group_ids})
+		return host_groups
 
 
 	def get_triggers(self, requestSince = None):
 		request_dict = {"jsonrpc":"2.0", "method":"trigger.get", "params":{"output":"extend", "monitored_hosts":True}, "auth":self.auth_token, "id":1}
 		if requestSince is not None:
 				request_dict["params"]["lastChangeSince"] = int(requestSince)
-		post = json.dumps(request_dict)
-		request = urllib2.Request(self.url, post, self.HEADER)
-		response = urllib2.urlopen(request)
-		res_str = response.read()
-		res_dict = json.loads(res_str)
 
 		self.result = check_response(res_dict)
 		if not self.result:
@@ -93,6 +82,14 @@ class ZabbixAPI:
 
 		triggers = []
 		for trigger in res_dict["result"]:
+
+
+def get_response_dict(request_dict, auth_key, params):
+	post = json.dumps(request_dict)
+	request = urllib2.Request(self.url, post, self.HEADER)
+	response = urllib2.urlopen(request)
+	res_str = response.read()
+	res_dict = json.loads(res_str)
 
 
 
