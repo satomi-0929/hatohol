59a0ba35656a52a2f4bc7e88f4f15d5b5d6914d6
Masayuki Nakagawa <masayuki.nakagawa@miraclelinux.com>
2015-05-07 17:58:10 +0900

    Delete indents

--- server/hap2/hapzabbix.py
+++ server/hap2/hapzabbix.py
@@ -7,40 +7,43 @@ import haplib
 import zabbixapi
 
 class HapZabbixProcedures(haplib.BaseProcedures):
-	def exchangeProfile(self, params):
-		optimize_server_procedures(self.valid_procedures_of_server, params)
-		my_procedures = haplib.get_implement_procedures(HapZabbixProcedures)
-		push_profile("response", my_procedures)
+    def exchangeProfile(self, params):
+        optimize_server_procedures(self.valid_procedures_of_server, params)
+        my_procedures = haplib.get_implement_procedures(HapZabbixProcedures)
+        push_profile("response", my_procedures)
 
 
-	def fetchItems(self):
-		print "Not implement"
+    def fetchItems(self):
+        print "Not implement"
 
 
-	def fetchHistory(self):
-		print "Not implement"
+    def fetchHistory(self):
+        print "Not implement"
 
 
-	def fetchTriggers(self):
-		print "Not implement"
+    def fetchTriggers(self):
+        print "Not implement"
 
 
-	def fetchEvents(self):
-		print "Not implement"
+    def fetchEvents(self):
+        print "Not implement"
 
 
 class PreviousHostsInfo:
-	self.hosts = []
-	self.host_groups = []
-	self.host_group_membeship = []
+    def __init__():
+        self.hosts = []
+        self.host_groups = []
+        self.host_group_membeship = []
+
+
 
 
 def routine_update():
-	print "Not implement"
+    print "Not implement"
 
 def main():
-	daemon_runner = daemon.runner.DaemonRunner()
+    daemon_runner = daemon.runner.DaemonRunner()
 
 
 if __name__ == '__main__':
-	main()
+    main()
