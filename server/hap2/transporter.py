549e8abd9df7b505358e68a84f7dfd858a174604
Kazuhiro Yamato <kz0817@gmail.com>
2015-05-20 12:10:44 +0900

    Add a new class: Transporter.

---
+++ server/hap2/transporter.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+"""
+  Copyright (C) 2015 Project Hatohol
+
+  This file is part of Hatohol.
+
+  Hatohol is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License, version 3
+  as published by the Free Software Foundation.
+
+  Hatohol is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with Hatohol. If not, see
+  <http://www.gnu.org/licenses/>.
+"""
+
+import logging
+
+class Transporter:
+    """
+    An abstract class for transportation of RPC messages for HAPI-2.0.
+    """
+
+    def __init__(self):
+        pass
+
+    def call(self, procedure, params, rpcid):
+        """
+        Call a RPC.
+        @param procedure  A name of the procedure.
+        @param params     An object used as 'params'.
+        @param rpcid      An ID of the call.
+        """
+        logging.debug("Called stub method: call().");
+
+    def reply(self, result, rpcid):
+        """
+        Replay a message to a caller.
+        @param result   An object used as 'result'.
+        @param rpc      An ID of the call.
+        """
+        logging.debug("Called stub method: reply().");
