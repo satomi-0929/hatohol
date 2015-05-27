35b45c63fc0dc16c92a84c9644dda2785c95b69e
Masayuki Nakagawa <masayuki.nakagawa@miraclelinux.com>
2015-05-27 17:21:25 +0900

    Add HAPBaseSender class test

--- server/hap2/test/TestHAPLib.py
+++ server/hap2/test/TestHAPLib.py
@@ -18,7 +18,85 @@
   <http://www.gnu.org/licenses/>.
 """
 import unittest
+import multiprocessing
 import haplib
+
+class TestHAPBaseSender(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.test_queue = multiprocessing.JoinableQueue()
+
+    def test_send_request_to_queue(self):
+        test_sender = SenderForTest(self.test_queue, True)
+        self.WheatherFunctionPasses(test_sender.send_request_to_queue,
+                                    "test_procedure_name", "test_param", 1)
+
+    def test_send_response_to_queue(self):
+        test_sender = SenderForTest(self.test_queue, True)
+        self.WheatherFunctionPasses(test_sender.send_response_to_queue,
+                                    "test_result", 1)
+
+    def test_send_error_to_queue(self):
+        test_sender = SenderForTest(self.test_queue, True)
+        self.WheatherFunctionPasses(test_sender.send_error_to_queue,
+                                    -32700, 1)
+
+    def test_get_response_and_check_id(self):
+        test_sender = SenderForTest(self.test_queue)
+        exact_result = "test_result"
+        exact_id = 1
+        self.test_queue.put({"id": exact_id, "result": exact_result})
+        self.test_queue.task_done()
+        output = test_sender.get_response_and_check_id(exact_id)
+
+        self.assertEquals(output, exact_result)
+
+    def test_get_response_and_check_id_timeout(self):
+        test_sender = SenderForTest(self.test_queue)
+        exact_result = "test_result"
+        test_id = 1
+        # Wait 30 minute
+        output = test_sender.get_response_and_check_id(test_id)
+
+        self.assertIsNone(output)
+
+    def test_get_monitoring_server_info(self):
+        test_sender = SenderForTest(self.test_queue, True)
+        self.WheatherFunctionPasses(test_sender.get_monitoring_server_info)
+
+    def test_get_last_info(self):
+        test_sender = SenderForTest(self.test_queue, True)
+        self.WheatherFunctionPasses(test_sender.get_last_info,
+                                    "test_element")
+
+    def test_exchange_profile_request(self):
+        test_sender = SenderForTest(self.test_queue, True)
+        self.WheatherFunctionPasses(test_sender.exchange_profile,
+                                    "test_params")
+
+    def test_exchange_profile_response(self):
+        test_sender = SenderForTest(self.test_queue, True)
+        self.WheatherFunctionPasses(test_sender.exchange_profile,
+                                    "test_params", 1)
+
+    def test_udpate_arm_info(self):
+        test_sender = SenderForTest(self.test_queue, True)
+        test_arm_info = haplib.ArmInfo()
+        self.WheatherFunctionPasses(test_sender.update_arm_info,
+                                    test_arm_info)
+
+    def WheatherFunctionPasses(self, func, *args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except Exception as exception:
+            print "Happened: " + str(exception)
+
+        if "exception" not in locals():
+            exception = None
+
+        assertIsNone(exception)
+
 
 class SenderForTest(haplib.HAPBaseSender):
 
