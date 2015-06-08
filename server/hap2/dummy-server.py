84a01b9e64c40b16f930db37052e33b093bfc189
Kazuhiro Yamato <kz0817@gmail.com>
2015-06-08 19:32:16 +0900

    [dummy-server] Implement somewhat.

--- server/hap2/dummy-server.py
+++ server/hap2/dummy-server.py
@@ -19,12 +19,35 @@
 """
 
 import haplib
+import multiprocessing
+import transporter
+import argparse
 
 class DummyServer:
 
+    def __init__(self, transporter_args):
+        procedures = ["getMonitoringServerInfo"]
+        self.__sender = haplib.Sender(transporter_args)
+        self.__rpc_queue = multiprocessing.JoinableQueue()
+        self.__receiver = haplib.DispatchableReceiver(transporter_args,
+                                                      self.__rpc_queue,
+                                                      procedures)
+
     def __call__(self):
-        pass
+        self.__receiver.daemonize()
+        while True:
+            request = self.__rpc_queue.get()
+            print request
 
 if __name__ == '__main__':
-    server = DummyServer()
+    parser = argparse.ArgumentParser()
+    haplib.Utils.define_transporter_arguments(parser)
+
+    args = parser.parse_args()
+    transporter_class = haplib.Utils.load_transporter(args)
+    transporter_args = {"class": transporter_class}
+    transporter_args.update(transporter_class.parse_arguments(args))
+    transporter_args["amqp_send_queue_suffix"] = "-T"
+    transporter_args["amqp_recv_queue_suffix"] = "-S"
+    server = DummyServer(transporter_args)
     server()
