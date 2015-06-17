a23ad65be1030eaab9d92ef733130196ce9d5f85
Kazuhiro Yamato <kz0817@gmail.com>
2015-06-17 19:36:51 +0900

    Add fetchEvents.

--- server/hap2/simple_caller.py
+++ server/hap2/simple_caller.py
@@ -28,32 +28,46 @@ class SimpleCaller:
         self.__sender = haplib.Sender(transporter_args)
         self.__COMMAND_HANDLERS = {
             "fetchTriggers": self.__rpc_fetch_triggers,
+            "fetchEvents":   self.__rpc_fetch_events,
         }
 
     def __call__(self, args):
         logging.info("Command: %s" % args.command)
         self.__curr_command = args.command
+        self.__curr_args = args
         self.__COMMAND_HANDLERS[args.command](args)
         self.__curr_command = None
+        self.__curr_args = None
 
     def __rpc_fetch_triggers(self, args):
-        host_ids = args.host_ids
-        if host_ids is None:
-            host_ids = ["1"]
-        params = {"fetchId": "1", "hostIds": host_ids}
+        params = {"hostIds": args.host_ids}
+        self.__request(params)
+
+    def __rpc_fetch_events(self, args):
+        params = {"lastInfo": args.last_info, "count": args.count,
+                  "direction": args.direction}
         self.__request(params)
 
     def __request(self, params):
         __component_code = 0
         request_id = haplib.Utils.generate_request_id(__component_code)
+        params["fetchId"] = self.__curr_args.fetch_id
         self.__sender.request(self.__curr_command, params, request_id)
 
     @staticmethod
     def arg_def(parser):
+        parser.add_argument('--fetch-id', default="1")
         subparsers = parser.add_subparsers(dest='command',
                                            help='sub-command help')
         parser_fetch_trig = subparsers.add_parser('fetchTriggers')
-        parser_fetch_trig.add_argument('--host-ids', nargs="+")
+        parser_fetch_trig.add_argument('--host-ids', nargs="+", default=["1"])
+
+        parser_fetch_evt = subparsers.add_parser('fetchEvents')
+        parser_fetch_evt.add_argument('--last-info', default="")
+        parser_fetch_evt.add_argument('--count', type=int, default=1000)
+        parser_fetch_evt.add_argument('--direction', choices=["ASC", "DESC"],
+                                      default="ASC")
+        parser_fetch_evt.add_argument('--fetch-id', default="1")
 
 
 if __name__ == '__main__':
