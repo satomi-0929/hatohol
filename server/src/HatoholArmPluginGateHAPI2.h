b1b0b4633942cccec691994e593bedcbdaf2c736
Takuro Ashie <ashie@clear-code.com>
2015-06-19 13:05:08 +0900

    [HatoholArmPluginGateHAPI2] Add launchProcess()

--- server/src/HatoholArmPluginGateHAPI2.h
+++ server/src/HatoholArmPluginGateHAPI2.h
@@ -61,6 +61,7 @@ public:
 protected:
 	virtual ~HatoholArmPluginGateHAPI2();
 	void upsertLastInfo(std::string lastInfoValue, LastInfoType type);
+	bool launchPluginProcess(const ArmPluginInfo &armPluginInfo);
 
 private:
 	std::string procedureHandlerExchangeProfile(JSONParser &parser);
